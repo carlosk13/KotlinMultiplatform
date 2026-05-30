c6f39b122aa01ce7b2f8e2b2c05447a60fdb2cdc
Ivan <133200juan@gmail.com>
2026-05-30 10:21:34 -0600

    fix: url ngrok correcta para pruebas iOS

--- composeApp/src/commonMain/kotlin/com/example/kotlinmultiplatform/core/constants/Constants.kt
+++ composeApp/src/commonMain/kotlin/com/example/kotlinmultiplatform/core/constants/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.kotlinmultiplatform.core.constants
 
 object Constants {
-    const val BASE_URL = "http://192.168.1.113:3000"
+    const val BASE_URL = "https://encourage-process-ascension.ngrok-free.dev"
     const val CONNECTION_TIMEOUT_SECONDS = 30L
     const val READ_TIMEOUT_SECONDS = 30L
 }
